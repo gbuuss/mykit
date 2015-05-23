e8707f334aa0e9be700a9f51d62f6b7ce6c5937a
Jesús Díaz <gbuuss@gmail.com>
2015-05-23 16:53:39 -0300

    Agregando active menu

--- js/kit.js
+++ js/kit.js
@@ -1,14 +1,16 @@
 Kit = function(){
 
     /**
-     * [cover description]
+     * Coloca una imagen de fondo a
+     * cualquier elemento.
+     * 
      * @return {[type]} [description]
      */
     var cover = function(){
-        var target = $('[data-mk="cover"]');
-        var image  = target.data("src");
+        var element = $('[data-mk="cover"]');
+        var image   = element.data("src");
 
-        target.css({
+        element.css({
             "background": "url("+image+") no-repeat center center",
             "-webkit-background-size": "cover",
             "-moz-background-size": "cover",
@@ -18,34 +20,58 @@ Kit = function(){
     }
 
     /**
-     * [scrollSmooth description]
+     * Mueve el scroll de la pagina
+     * hasta una seccion ID.
+     * 
      * @return {[type]} [description]
      */
     var scrollSmooth = function(){
-        $('a[href*=#]:not([href=#])').click(function() {
+        $('a[href*=#]:not([href=#])').on("click", function(){
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
-                    $('html,body').animate({
+                    $('html, body').animate({
                         scrollTop: target.offset().top
                     }, 1000);
                     return false;
                 }
             }
+        })
+    }
+
+    /**
+     * [menu description]
+     * @return {[type]} [description]
+     */
+    var menu = function(){
+        var element = $('[data-mk="menu"]');
+        element.find('a').on("click", function(){
+            element.find("li").removeClass("active");
+            $(this).parents("li").addClass("active");
         });
     }
 
     return {
+
+        /**
+         * Inicializa la configuracion global
+         * del framework.
+         * 
+         * @param  {[type]} options [description]
+         * @return {[type]}         [description]
+         */
         init: function(options){
 
-            // These are the defaults functions.
+            // funciones por defecto
             var settings = $.extend({
                 cover: false,
-                scrollSmooth: false
+                menu: false,
+                scrollSmooth: false,
             }, options);
 
-            // Calling functions
+            // inicializa las funciones
+            settings.menu == true && menu();
             settings.cover == true && cover();
             settings.scrollSmooth == true && scrollSmooth();
         }
