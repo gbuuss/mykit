7c8d64837df362cbef0db0ea08c6af5781348511
Jesús Díaz <gbuuss@gmail.com>
2015-05-23 13:22:48 -0300

    Separando archivos less

--- js/kit.js
+++ js/kit.js
@@ -1,21 +1,29 @@
 Kit = function(){
 
     var cover = function(){
-        var target = $('[data-mk="cover"]').parent();
-        var image  = $('[data-mk="cover"]').attr("src");
+        var target = $('[data-mk="cover"]');
+        var image  = target.data("src");
 
         target.css({
             "background": "url("+image+") no-repeat center center",
-            "-webkit-background-size": cover,
-            "-moz-background-size": cover,
-            "-o-background-size": cover,
-            "background-size": cover,
+            "-webkit-background-size": "cover",
+            "-moz-background-size": "cover",
+            "-o-background-size": "cover",
+            "background-size": "cover",
         });
     }
 
     return {
-        init: function(){
-            cover();
+        init: function(options){
+
+            // These are the defaults.
+            var settings = $.extend({
+                cover: false,
+            }, options);
+
+            if(settings.cover){
+                cover();
+            }
         }
     }
 }();
