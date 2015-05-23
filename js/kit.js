508c5c342d603313db36b2dab086296329f689db
Jesús Díaz <gbuuss@gmail.com>
2015-05-23 20:07:36 -0300

    Marcando link al scrollear

--- js/kit.js
+++ js/kit.js
@@ -20,35 +20,49 @@ Kit = function(){
     }
 
     /**
-     * Mueve el scroll de la pagina
-     * hasta una seccion ID.
-     * 
-     * @return {[type]} [description]
-     */
-    var scrollSmooth = function(){
-        $('a[href*=#]:not([href=#])').on("click", function(){
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        })
-    }
-
-    /**
      * [menu description]
      * @return {[type]} [description]
      */
     var menu = function(){
         var element = $('[data-mk="menu"]');
-        element.find('a').on("click", function(){
+        var smooth  = element.data("smooth");
+
+        element.find('a[href*=#]:not([href=#])').on("click", function(){
+
+            // marcando como activo el link presionado
             element.find("li").removeClass("active");
             $(this).parent().addClass("active");
+
+            // scroll smoth
+            if(smooth === "on"){
+                if(location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname){
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    if (target.length) {
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 1000);
+                        return false;
+                    }
+                }
+            }
+        });
+
+        // marcando menu cuando se scrollea
+        $(document).on("scroll", function(){
+            var scrollPos = $(document).scrollTop();
+            element.find("a[href*=#]:not([href=#])").each(function () {
+                var currLink   = $(this);
+                var refElement = $(currLink.attr("href"));
+                if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                    //$('#menu-center ul li a').removeClass("active");
+                    element.find("li").removeClass("active");
+                    currLink.parent().addClass("active");
+                }
+                else{
+                    currLink.parent().removeClass("active");
+                }
+            });
         });
     }
 
@@ -67,13 +81,11 @@ Kit = function(){
             var settings = $.extend({
                 cover: false,
                 menu: false,
-                scrollSmooth: false,
             }, options);
 
             // inicializa las funciones
             settings.menu == true && menu();
             settings.cover == true && cover();
-            settings.scrollSmooth == true && scrollSmooth();
         }
     }
 }();
